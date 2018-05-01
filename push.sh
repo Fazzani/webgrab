d9e222b1b2a75e2fd6aba5e35da2dd0216015637
Fazzani <tunisienheni@outlook.com>
2018-05-01 20:07:40 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+git config --global user.email "fazzani.heni@outlook.com"
+git config --global user.name "fazzani"
+  
 tar -cvf xmltv.tar $1/*.xml
 git add --all
 git commit -m "daily commit $(date +%d/%m/%y)" 
