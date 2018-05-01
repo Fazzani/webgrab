12928b39dc2aa8a12ace061ae20c65f7ef910a11
Fazzani <tunisienheni@outlook.com>
2018-05-01 20:04:48 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,8 +2,7 @@
 
 set -eux
 
-cd $1
-tar -cvf xmltv.tar ./*.xml
-cp xmltv.tar /webgrab/xmltv.tar
-cd webgrab git add --all
-git commit -m "daily commit $(date +%d/%m/%y)" git push
+tar -cvf xmltv.tar $1/*.xml
+git add --all
+git commit -m "daily commit $(date +%d/%m/%y)" 
+git push
