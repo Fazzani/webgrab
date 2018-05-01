a7d30e722c32b0af3353b06006aa0bf5bf89248f
Fazzani <tunisienheni@outlook.com>
2018-05-01 20:02:34 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -4,7 +4,6 @@ set -eux
 
 cd $1
 tar -cvf xmltv.tar ./*.xml
-git clone https://github.com/Fazzani/xmltv.git
-cp xmltv.tar $1/xmltv.tar
-cd xmltv git add --all
+cp xmltv.tar /webgrab/xmltv.tar
+cd webgrab git add --all
 git commit -m "daily commit $(date +%d/%m/%y)" git push
