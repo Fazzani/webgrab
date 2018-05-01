9ce4c0e03f2890379431e07cb9517f7c5f0ac015
Fazzani <tunisienheni@outlook.com>
2018-05-01 20:22:44 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-set -eux
+# example to use with docker
+# docker run -ti --rm --name xmltv -v /mnt/nfs/webgrab:/root -v $HOME/.ssh:/root/.ssh pallet/git-client bash -c \
+# "git clone https://github.com/Fazzani/webgrab.git /webgrab && cd /webgrab && chmod +x /webgrab/push.sh && /webgrab/push.sh /root/xmltv"
 
-git config --global user.email "fazzani.heni@outlook.com"
-git config --global user.name "fazzani"
-  
+set -eux
+#git config --global user.email "fazzani.heni@outlook.com"
+#git config --global user.name "fazzani"
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
 tar -cvf xmltv.tar $1/*.xml
 git add --all
 git commit -m "daily commit $(date +%d/%m/%y)" 
